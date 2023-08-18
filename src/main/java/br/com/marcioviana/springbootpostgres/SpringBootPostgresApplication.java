0e68b1f7df1d2b4a6eea9f57a72127710a881317
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-08-18 17:29:19 -0300

    feat: Primeiro commit. Criar projeto/ application.properties/ docker-compose.yaml

---
+++ src/main/java/br/com/marcioviana/springbootpostgres/SpringBootPostgresApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.springbootpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcioviana/springbootpostgres/SpringBootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.springbootpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
